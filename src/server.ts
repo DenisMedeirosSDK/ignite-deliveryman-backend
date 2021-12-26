68dc0683ae29a39e202f8452ee74693ca43ca553
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-12-26 20:59:07 -0300

    feat: create clients and save in database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { routes } from './routes';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({
