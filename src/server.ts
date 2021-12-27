9c41e76452b4949f7828bfc642c3e92120abff06
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-12-27 15:05:42 -0300

    feat: create exception errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 const PORT = process.env.PORT || 3333;
@@ -14,6 +15,21 @@ app.get('/', (request, response) => {
     message: 'Hello World!!',
   });
 });
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log('Server start on PORT');
