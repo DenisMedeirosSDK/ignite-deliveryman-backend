d30e94ff4781ce1eb8dbff27e34713ccf65464c1
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-12-27 15:04:08 -0300

    feat: create auth with jwt

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { AuthenticateClientController } from './modules/acount/authenticateUser/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
+routes.post('/authenticate', authenticateClientController.handle);
 routes.post('/client/', createClientController.handle);
 
 export { routes };
